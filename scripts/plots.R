7ffe5fb7f768c9cef0fb2a0a7dfb1187bb3a94c1
PoryPany <albertg952@gmail.com>
2021-05-28 09:28:04 +0200

    Dodanie mapy

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,10 +1,10 @@
 require("ggplot2")
 
 # Plot#1, showing mortality by age groups
-yearlyData <- yearlyDeathsByGroups(A2020)
+yearlyData <- yearlyDeathsByGroups(A2021)
 ggplot(yearlyData, aes(Age, R)) + geom_bar(stat = "identity") + 
   labs(title = "2020 Deaths", x = "Age group", y = "Deaths") +
-  ggsave("plot#1.png", width = 13, height = 7)
+  ggsave("plots/plot#1.png", width = 13, height = 7)
 
 
 # Plot#2, showing weekly mortality for specific years
@@ -20,7 +20,7 @@ Weeks <- factor(test$Week, labels = 1:53)
 ggplot(test, aes(x = Weeks, y = Deaths, 
                  group = Year, color = Year)) +
   geom_line(size = 3) + ylim(0, max(test$Deaths)) +
-  ggsave("plot#2.png", width = 13, height = 7)
+  ggsave("plots/plot#2.png", width = 13, height = 7)
 
 
 # Plot#3, map
@@ -58,4 +58,5 @@ ggplot(data = PolandMapData) +
   scale_fill_viridis_c(alpha = 1, begin = 0, end = 1, direction = -1,
                        option = "D", values = NULL, space = "Lab",
                        guide = "colourbar", aesthetics = "colour") +
-  coord_sf(xlim = c(13,25), ylim = c(48.75, 55), expand = F)
+  coord_sf(xlim = c(13,25), ylim = c(48.75, 55), expand = F) +
+  ggsave("plots/plot#3.png", width = 7, height = 7)
